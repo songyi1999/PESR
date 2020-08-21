f5a05422c5f1a403f9cbc5c4c10a16fff2735dfb
songyi <songyi1999@users.noreply.github.com>
2020-08-21 23:08:18 +0800

    Update focal_loss.py

--- model/focal_loss.py
+++ model/focal_loss.py
@@ -10,4 +10,4 @@ class FocalLoss(nn.Module):
         p = x.sigmoid()
         pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
         w = (1-pt).pow(self.gamma)
-        return F.binary_cross_entropy_with_logits(x, t, w)
+        return F.binary_cross_entropy_with_logits(x, t, w.detach() )
