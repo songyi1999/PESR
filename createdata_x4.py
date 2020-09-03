5bb551d39812e766df64c86eb40e01bff5f8e930
songyi <songyi1999@users.noreply.github.com>
2020-09-03 23:19:52 +0800

    Update createdata_x4.py

--- createdata_x4.py
+++ createdata_x4.py
@@ -1,3 +1,4 @@
+import os
 model_base_path='/content/drive/Shared drives/songyi1999/PESR/check_point/my_model/train/'
 dataset_list=['Set5','Set14']
 save_base_path='/content/drive/Shared drives/songyi1999/PESR/results/'
@@ -7,3 +8,4 @@ for dataset in  dataset_list:
       num=i+1
       cmd="python test.py --perceptual_model '{}' --psnr_model '/content/drive/Shared drives/songyi1999/PESR/check_point/my_model/pretrain/best_model.pt' --save_path '{}'".format(model_base_path+"model_"+str(num)+".pt" ,save_base_path+dataset+"/"+str(num)   )
       print(cmd)
+      os.system(cmd)
